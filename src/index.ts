89f11dfdb91e120d82df60bfbb33b28ce1f1fc11
taoqf <tao_qf@163.com>
2020-03-11 17:46:04 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import moment from 'moment';
 
-export default function an12(param1: string, param2: string) {
+/**
+ * 获取某天的结束时间
+ * @param time 时间戳
+ */
+export default function end_day(time: string, mask: string): number;
+export default function end_day(time: number): number;
+export default function end_day(time: number | string, mask?: string) {
+	return moment(time, mask).endOf('day').valueOf();
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,18 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+
+test('获取一天的结束时间 string', (t) => {
+	const end = a('2019/09/18', 'YYYY/MM/DD');
+	t.is(end, 1568822399999);
+});
+
+test('获取一天的结束时间 number 1', (t) => {
+	const end = a(15688223000000);
+	t.is(end, 15688223999999);
+});
+
+test('获取一天的结束时间 number 2', (t) => {
+	const end = a(15688223999999);
+	t.is(end, 15688223999999);
 });
